76445d5d39b8e66cab76e777f4d524ba5fd425a1
Robson Vargas <robsonsvargas1993@gmail.com>
2021-10-28 11:01:36 -0300

    Comparable on TreeSet

--- src/set/equalitytest/set/application/Program.java
+++ src/set/equalitytest/set/application/Program.java
@@ -9,10 +9,13 @@ public class Program {
 
 	public static void main(String[] args) {
 		Set<Product> set = new HashSet<>();
+		
 		set.add(new Product("TV", 900.0));
 		set.add(new Product("Notebook", 1200.0));
 		set.add(new Product("Tablet", 400.0));
+		
 		Product prod = new Product("Notebook", 1200.0);
+		
 		System.out.println(set.contains(prod));
 		}
 
